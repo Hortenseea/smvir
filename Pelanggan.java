96d9de7a8c702c9a3ca28439f4511ef838f05d93
Annurman <150548765+Annurman@users.noreply.github.com>
2026-06-24 08:36:27 +0700

    add coment

--- Pelanggan.java
+++ Pelanggan.java
@@ -1,23 +1,23 @@
-public class Pelanggan {
-    private String id;
-    private String nama;
-    private String nomorHP;
+public class Pelanggan { //buat class pelanggan
+    private String id;  //buat variabel id bertipe data string dsengan acces modifer private
+    private String nama;    //buat variabel nama bertipe data string dsengan acces modifer private
+    private String nomorHP;    //buat variabel nomorHP bertipe data string dsengan acces modifer private
 
-    public Pelanggan(String id, String nama, String nomorHP) {
-        this.id = id;
-        this.nama = nama;
-        this.nomorHP = nomorHP;
+    public Pelanggan(String id, String nama, String nomorHP) { //buat constructor pelanggan dengan parameter id,nama,nomorHP
+        this.id = id;  //isi variabel class id dengan variabel parameter id
+        this.nama = nama;    //isi variabel class nama dengan variabel parameter nama
+        this.nomorHP = nomorHP;    //isi variabel class id dengan variabel parameter id
     }
 
-    public String getId() {
+    public String getId() {  //buat getter untuk atribur id
         return id;
     }
 
-    public String getNama() {
+    public String getNama() { //buat getter untuk atribur nama
         return nama;
     }
 
-    public String getNomorHP() {
+    public String getNomorHP() {    //buat getter untuk atribur nomorHP
         return nomorHP;
     }
 }
